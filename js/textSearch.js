82b64d84bd8bb143344bbec58001a0f80951fbf6
Ellika Larsson <ellika.larsson2@gmail.com>
2024-03-01 23:15:01 +0100

    add search bar and function for finding a planet

--- js/textSearch.js
+++ js/textSearch.js
@@ -1,11 +1,12 @@
 import { getPlanetInfo } from "./fetchAPI.js";
 
 export async function searchPlanets(searchString) {
+    console.log(searchString)
+    let planets = await getPlanetInfo();
+    let lowerCaseSearchString = searchString.toLowerCase();
 
-let planets = await getPlanetInfo();
-let lowerCaseSearchString = searchString.toLowerCase();
 
-
-return  planets.bodies.find(body =>
-    body.name.toLowerCase().includes(lowerCaseSearchString));
+    return planets.bodies.find(body =>
+        body.name.toLowerCase() === lowerCaseSearchString
+    );
 }
--- main.js
+++ main.js
@@ -1,3 +1,41 @@
 import { planetEventListner } from "./js/planetClick.js";
+import {searchPlanets} from "./js/textSearch.js";
 planetEventListner();
 
+const searchinput = document.querySelector("#searchbar__input"); 
+const formSearch = document.querySelector("#searchbar");
+
+formSearch.addEventListener('submit', async function(event){
+    event.preventDefault();
+    let planet = await searchPlanets(searchinput.value);
+    document.querySelector('#planet-name').innerText = planet.name;
+    document.querySelector('#latin-name').innerText = planet.latinName;
+    document.querySelector('#desc').innerText = planet.desc;
+    document.querySelector('#circumference').innerText = planet.circumference;
+    document.querySelector('#sun-distance').innerText = planet.distance;
+
+    let maxTempElement = document.querySelector('#maxTemp');
+    let minTempElement = document.querySelector('#minTemp');
+    maxTempElement.innerText = planet.temp.day;
+    minTempElement.innerText = planet.temp.night;   
+   
+
+    const moonList = document.querySelector('#moon-list');
+    moonList.innerHTML = '';
+    planet.moons.forEach(moon => {
+        let moonItem = document.createElement('li');
+        moonItem.setAttribute ('class','moon-block__list')
+        moonItem.innerText = moon;
+        moonList.appendChild(moonItem);
+
+       
+    });
+
+    window.scrollTo({left: 0, top: document.body.scrollHeight, behavior: "smooth"});
+
+    let sun = document.querySelector('.sun');
+    sun.className = 'sun'
+    sun.classList.add(`sun-${planet.name.toLowerCase()}`);
+    searchinput.value = ""
+
+})
